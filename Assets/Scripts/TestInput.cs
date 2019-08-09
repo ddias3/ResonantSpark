330704a5ee53162095186f75b5623658fedd473a
Daniel Dias <danieldias15@gmail.com>
2019-08-08 21:14:12 -0400

    Debug Direciton Input Screen

--- Assets/Scripts/TestInput.cs
+++ Assets/Scripts/TestInput.cs
@@ -10,7 +10,8 @@ public class TestInput : MonoBehaviour {
 
     public InputBuffer inputBuffer;
 
-    public Image arrow;
+    public Image immediateArrow;
+    public Image bufferArrow;
 
     private int horizontalInput = 0;
     private int verticalInput = 0;
@@ -37,11 +38,20 @@ public class TestInput : MonoBehaviour {
         if (Input.GetKey(KeyCode.D)) horizontalInput += 1;
 
         if (horizontalInput == 0 && verticalInput == 0) {
-            arrow.enabled = false;
+            immediateArrow.enabled = false;
         }
         else {
-            arrow.enabled = true;
-            arrow.GetComponent<Transform>().rotation = Quaternion.Euler(0.0f, 0.0f, Mathf.Atan2(verticalInput, horizontalInput) * Mathf.Rad2Deg - 90.0f);
+            immediateArrow.enabled = true;
+            //immediateArrow.GetComponent<Transform>().rotation = Quaternion.Euler(0.0f, 0.0f, Mathf.Atan2(verticalInput, horizontalInput) * Mathf.Rad2Deg - 90.0f);
+            immediateArrow.GetComponent<Transform>().rotation = Quaternion.Euler(0.0f, 0.0f, Mathf.Atan2(-horizontalInput, verticalInput) * Mathf.Rad2Deg);
+        }
+
+        if (inputBuffer.GetLatestInput() == FightingGameInputCodeDir.Neutral || inputBuffer.GetLatestInput() == FightingGameInputCodeDir.None) {
+            bufferArrow.enabled = false;
+        }
+        else {
+            bufferArrow.enabled = true;
+            bufferArrow.GetComponent<Transform>().rotation = Quaternion.Euler(0.0f, 0.0f, Mathf.Atan2((((int) inputBuffer.GetLatestInput()) - 1) / 3 - 1, (((int) inputBuffer.GetLatestInput()) - 1) % 3 - 1) * Mathf.Rad2Deg - 90.0f);
         }
 
         inputBuffer.AddInput((FightingGameInputCodeDir) ((verticalInput + 1) * 3 + (horizontalInput + 1) + 1));
