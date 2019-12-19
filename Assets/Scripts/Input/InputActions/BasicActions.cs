65f68839b669804dcc3faad0fc29b1c3cedff154
Daniel Dias <danieldias15@gmail.com>
2019-12-19 18:57:16 -0500

    Added new InputAction system to project

---
+++ Assets/Scripts/Input/InputActions/BasicActions.cs
@@ -0,0 +1,315 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/InputActions/BasicActions.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+namespace ResonantSpark
+{
+    public class @BasicActions : IInputActionCollection, IDisposable
+    {
+        private InputActionAsset asset;
+        public @BasicActions()
+        {
+            asset = InputActionAsset.FromJson(@"{
+    ""name"": ""BasicActions"",
+    ""maps"": [
+        {
+            ""name"": ""GamePlay"",
+            ""id"": ""081761cf-b8b6-4282-8428-dec7e0e43be2"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""908f578c-b4b3-4558-b244-e9c99b000415"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Fire"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""0af61e80-070d-4356-88bf-2e37ddf84a78"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""6457e8c5-3c7c-491f-8029-4726ce1547b4"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Gamepad"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""cdc23752-97e4-4788-acaa-4b8caee1fb6d"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""0308b7c5-e860-44e6-835f-3fdaced812e5"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""97c55b7b-25b2-471e-84d0-945c0524e267"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""c06ff365-476f-4f69-a5aa-915ae1604c5b"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""f7aefa0c-a14e-4510-a42a-d980ce2736a4"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""f3ed63da-2e86-47c0-a5e1-2d4adc0f9fa5"",
+                    ""path"": ""<Gamepad>/rightStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Gamepad"",
+                    ""action"": ""Fire"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""ArrowKeys"",
+                    ""id"": ""d5db8765-98fe-4a96-bc8b-cd791231fe3c"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Fire"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""7c59cf04-529e-4ba7-a76c-9523d0ec522f"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Fire"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""5110a0a8-590e-4478-8e53-5a6bd18a9466"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Fire"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""127a19e3-8400-4420-a1f7-14b61530b9ff"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Fire"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""3ba1088b-2af1-405e-b220-01b3ad27a9c2"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Fire"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Keyboard"",
+            ""bindingGroup"": ""Keyboard"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        },
+        {
+            ""name"": ""Gamepad"",
+            ""bindingGroup"": ""Gamepad"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Gamepad>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+            // GamePlay
+            m_GamePlay = asset.FindActionMap("GamePlay", throwIfNotFound: true);
+            m_GamePlay_Move = m_GamePlay.FindAction("Move", throwIfNotFound: true);
+            m_GamePlay_Fire = m_GamePlay.FindAction("Fire", throwIfNotFound: true);
+        }
+
+        public void Dispose()
+        {
+            UnityEngine.Object.Destroy(asset);
+        }
+
+        public InputBinding? bindingMask
+        {
+            get => asset.bindingMask;
+            set => asset.bindingMask = value;
+        }
+
+        public ReadOnlyArray<InputDevice>? devices
+        {
+            get => asset.devices;
+            set => asset.devices = value;
+        }
+
+        public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+        public bool Contains(InputAction action)
+        {
+            return asset.Contains(action);
+        }
+
+        public IEnumerator<InputAction> GetEnumerator()
+        {
+            return asset.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Enable()
+        {
+            asset.Enable();
+        }
+
+        public void Disable()
+        {
+            asset.Disable();
+        }
+
+        // GamePlay
+        private readonly InputActionMap m_GamePlay;
+        private IGamePlayActions m_GamePlayActionsCallbackInterface;
+        private readonly InputAction m_GamePlay_Move;
+        private readonly InputAction m_GamePlay_Fire;
+        public struct GamePlayActions
+        {
+            private @BasicActions m_Wrapper;
+            public GamePlayActions(@BasicActions wrapper) { m_Wrapper = wrapper; }
+            public InputAction @Move => m_Wrapper.m_GamePlay_Move;
+            public InputAction @Fire => m_Wrapper.m_GamePlay_Fire;
+            public InputActionMap Get() { return m_Wrapper.m_GamePlay; }
+            public void Enable() { Get().Enable(); }
+            public void Disable() { Get().Disable(); }
+            public bool enabled => Get().enabled;
+            public static implicit operator InputActionMap(GamePlayActions set) { return set.Get(); }
+            public void SetCallbacks(IGamePlayActions instance)
+            {
+                if (m_Wrapper.m_GamePlayActionsCallbackInterface != null)
+                {
+                    @Move.started -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnMove;
+                    @Move.performed -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnMove;
+                    @Move.canceled -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnMove;
+                    @Fire.started -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnFire;
+                    @Fire.performed -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnFire;
+                    @Fire.canceled -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnFire;
+                }
+                m_Wrapper.m_GamePlayActionsCallbackInterface = instance;
+                if (instance != null)
+                {
+                    @Move.started += instance.OnMove;
+                    @Move.performed += instance.OnMove;
+                    @Move.canceled += instance.OnMove;
+                    @Fire.started += instance.OnFire;
+                    @Fire.performed += instance.OnFire;
+                    @Fire.canceled += instance.OnFire;
+                }
+            }
+        }
+        public GamePlayActions @GamePlay => new GamePlayActions(this);
+        private int m_KeyboardSchemeIndex = -1;
+        public InputControlScheme KeyboardScheme
+        {
+            get
+            {
+                if (m_KeyboardSchemeIndex == -1) m_KeyboardSchemeIndex = asset.FindControlSchemeIndex("Keyboard");
+                return asset.controlSchemes[m_KeyboardSchemeIndex];
+            }
+        }
+        private int m_GamepadSchemeIndex = -1;
+        public InputControlScheme GamepadScheme
+        {
+            get
+            {
+                if (m_GamepadSchemeIndex == -1) m_GamepadSchemeIndex = asset.FindControlSchemeIndex("Gamepad");
+                return asset.controlSchemes[m_GamepadSchemeIndex];
+            }
+        }
+        public interface IGamePlayActions
+        {
+            void OnMove(InputAction.CallbackContext context);
+            void OnFire(InputAction.CallbackContext context);
+        }
+    }
+}
