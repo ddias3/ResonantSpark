1d39495a8b15aa9ffcabe998f0f20c8231f26485
Daniel Dias <danieldias15@gmail.com>
2019-12-30 22:14:07 -0500

    Updated InputSystem to update manually rather than dynamically

---
+++ Assets/Scripts/Input/UpdateInput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace ResonantSpark {
+    namespace Input {
+        [RequireComponent(typeof(FrameEnforcer))]
+        public class UpdateInput : MonoBehaviour {
+            private FrameEnforcer frame;
+
+            public void Start() {
+                frame = gameObject.GetComponentInParent<FrameEnforcer>();
+                frame.SetUpdate(new Action<int>(ProcessEventsManually));
+            }
+
+            public void ProcessEventsManually(int frameIndex) {
+                InputSystem.Update();
+            }
+        }
+    }
+}
