18143ce3553ff4b18df6c3a47c45fcd723a03e09
Daniel Dias <danieldias15@gmail.com>
2020-01-09 14:43:22 -0500

    Added hurtbox to the character prefab

---
+++ Assets/Scripts/Gameplay/HurtBox.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ResonantSpark {
+    namespace Gameplay {
+        public class HurtBox : MonoBehaviour {
+            public string hurtBoxName;
+
+            private FightingGameCharacter fgChar;
+            private LayerMask hurtBox;
+            private LayerMask hitBox;
+
+            public void Start() {
+                fgChar = GetComponentInParent<FightingGameCharacter>();
+                hurtBox = LayerMask.NameToLayer("HurtBox");
+                hitBox = LayerMask.NameToLayer("HitBox");
+            }
+
+            public void OnTriggerEnter(Collider other) {
+                if (other.gameObject.layer == hitBox) {
+                    Debug.LogFormat("Hit hurtBox {0}", hurtBoxName);
+                    // TODO: Call callback on fgChar;
+                    //      fgChar.OnHit();
+                }
+            }
+        }
+    }
+}
