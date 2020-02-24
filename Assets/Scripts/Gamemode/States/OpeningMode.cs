955783f2cb70ebf9e8f52923ad2a4af9c6a08858
Nathan Florez <nflorez89@gmail.com>
2020-02-23 20:05:23 -0500

    skeleton code for the following states added: Load in, Opening, Round End, Game End.

---
+++ Assets/Scripts/Gamemode/States/OpeningMode.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using ResonantSpark.Utility;
+using ResonantSpark.Gamemode;
+
+namespace ResonantSpark
+{
+    namespace GamemodeStates
+    {
+        public class OpeningMode : BaseState
+        {
+            private OneOnOneRoundBased oneOnOne;
+            private GameTimeManager gameTimeManager;
+            float elapsedTime;
+
+            // Use this for initialization
+            void Awake()
+            {
+                base.Awake();
+                oneOnOne = gameObject.GetComponentInParent<OneOnOneRoundBased>();
+                gameTimeManager = GameObject.FindGameObjectWithTag("rspTime").GetComponent<GameTimeManager>();
+            }
+
+            public override void Enter(int frameIndex, IState previousState)
+            {
+                elapsedTime = 0;
+            }
+
+            public override void Execute(int frameIndex)
+            {
+                elapsedTime += gameTimeManager.Layer("gameTime");
+            }
+
+            public override void Exit(int frameIndex)
+            { }
+        }
+    }
+}
