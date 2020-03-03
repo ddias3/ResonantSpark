44a24dadd719c57a66d105153a7fd6983658668a
Daniel Dias <danieldias15@gmail.com>
2020-03-03 13:42:32 -0500

    TestMenu now works, kind of

---
+++ Assets/Scripts/Input/UpdateInputMenuScenes.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace ResonantSpark {
+    namespace Input {
+        public class UpdateInputMenuScenes : MonoBehaviour {
+            public void FixedUpdate() {
+                InputSystem.Update();
+            }
+        }
+    }
+}
