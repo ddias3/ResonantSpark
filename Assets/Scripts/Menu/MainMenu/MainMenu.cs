1a3b1ff57a5acb62ddf25c8e25378f67719323d7
Daniel Dias <danieldias15@gmail.com>
2020-04-09 01:15:58 -0400

    Created menus basic buttons

--- Assets/Scripts/Menu/MainMenu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu/MainMenu.cs
@@ -1,36 +1,17 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-using ResonantSpark.Service;
-
 namespace ResonantSpark {
     namespace Menu {
         public class MainMenu : MonoBehaviour {
-            public TMPro.TMP_InputField char0;
-            public TMPro.TMP_InputField char1;
-            public TMPro.TMP_InputField level;
-            public void OnPressLoad() {
-                string strChar0 = "male0", strChar1 = "male0", strLevel = "corporation";
 
-                if (char0.text != "") {
-                    strChar0 = char0.text;
-                }
+            public GameObject mainMenu;
+            public GameObject optionsMenu;
+            public GameObject soundMenu;
+            public GameObject creditsMenu;
 
-                if (char1.text != "") {
-                    strChar1 = char1.text;
-                }
-
-                if (level.text != "") {
-                    strLevel = level.text;
-                }
-
-                Persistence persistence = Persistence.GetPersistence();
-
-                persistence.gamemode = "oneOnOneRoundBased";
-                persistence.SetCharacterSelected(0, strChar0);
-                persistence.SetCharacterSelected(1, strChar1);
-
-                SceneManager.LoadScene(strLevel, LoadSceneMode.Single);
+            public void Update() {
+                
             }
         }
     }
